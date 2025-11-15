aea2285416f900db174e39758a5bc0bee615e11e
Sarthak Acharya <sarthakacharya220@gmail.com>
2025-11-15 22:01:55 +0545

    Add roomspace and member models

---
+++ backend/models/roomspace.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// Roomspace represents a shared living space
+type Roomspace struct {
+	ID          uint           `gorm:"primaryKey" json:"id"`
+	Name        string         `gorm:"not null" json:"name"`
+	Description string         `json:"description,omitempty"`
+	CreatedBy   string         `gorm:"not null" json:"created_by"` // Firebase UID
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
+	Members     []RoomspaceMember `gorm:"foreignKey:RoomspaceID" json:"members,omitempty"`
+}
+
+// RoomspaceMember represents the many-to-many relationship between users and roomspaces
+type RoomspaceMember struct {
+	ID          uint      `gorm:"primaryKey" json:"id"`
+	RoomspaceID uint      `gorm:"not null" json:"roomspace_id"`
+	FirebaseUID string    `gorm:"not null" json:"firebase_uid"`
+	JoinedAt    time.Time `json:"joined_at"`
+}
+
+// CreateRoomspaceRequest represents the request to create a roomspace
+type CreateRoomspaceRequest struct {
+	Name        string `json:"name" binding:"required"`
+	Description string `json:"description,omitempty"`
+}
+
+// JoinRoomspaceRequest represents the request to join a roomspace
+type JoinRoomspaceRequest struct {
+	RoomspaceID string `json:"roomspace_id" binding:"required"`
+}
